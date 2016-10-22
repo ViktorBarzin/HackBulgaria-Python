a4558b12ad84bfe446904d5bf8af7b7dc62fb61b
ViktorBarzin <vbarzin@gmail.com>
2016-10-22 17:55:52 +0300

    reduce file path

--- week-1/1-bonus_tasks.py
+++ week-1/1-bonus_tasks.py
@@ -1,3 +1,6 @@
+import re
+
+
 def are_anagrams(s1, s2):
     # works for current purpose, but I guess there are more efficient solutions
     return hash(str(sorted(s1.lower()))) == hash(str(sorted(s2.lower())))
@@ -15,3 +18,19 @@ def gas_stations(distance, tank_size, stations):
             size = tank_size
     return res[1:]
 
+
+def reduce_file_path(path):
+    print(path)
+    splited = path.split('/')
+    result = []
+    for x in splited:
+        if x != '.' and x != '' and x != '..':
+            result.append(x)
+        elif x == '..' and len(result) > 0:
+            result.pop()
+
+    if len(result) == 0:
+        return '/'
+    elif len(result) == 1 and re.match('[a-zA-Z]', result[0]):
+        return '/' + result[0]
+    return '/' + '/'.join(result)
