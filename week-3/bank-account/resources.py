87ae6894a25cb4be00f93039001248a65b283104
ViktorBarzin <vbarzin@gmail.com>
2016-11-02 18:54:05 +0200

    bank account solution

---
+++ week-3/bank-account/resources.py
@@ -0,0 +1,19 @@
+NEGATIVE_NUMBER_ERROR_MESSAGE = 'Negative number is prohibited'
+NOT_BANK_ACCOUNT_TYPE_ERROR_MESSAGE = 'The provided argument is not from BankAccount class'
+NEGATIVE_MONEY_ERROR_MESSAGE = 'You cannot give more than you have b*!%&.'
+DIFFERENT_CURRENCIES_ERROR_MESSAGE = 'Cannot do operations on different currencies!'
+ACCOUNT_CREATED_MESSAGE = 'Account was created'
+
+INT_CHECK_MESSAGE = '__int__ check -> '
+WITHDRAW_MESSAGE = 'Withdraw for '
+FAILED = ' failed'
+WAS_WITHDRAWED = ' was withdrawed.'
+DEPOSITED = 'Deposited'
+
+# Transfer constants
+TRANSFER = 'Transfer'
+TO = 'to'
+FROM = 'from'
+FOR = 'for'
+
+BALANCE_CHECK = 'Balance check -> '
